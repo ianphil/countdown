bcaa46f7ff6de957dc1a39c5bac7406aae9f9ea8
tripdubroot <ian.philpot@microsoft.com>
2018-01-19 11:27:40 -0500

    deploy stuffs

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,6 @@ FROM nginx:alpine
 
 WORKDIR /usr/share/nginx/html
 COPY ./ .
+
+# tripdubroot/countdown:0.0.1-alpine
+# docker run -d -p 80:80 tripdubroot/countdown:0.0.1-alpine
---
+++ scripts/azdeploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker push tripdubroot/countdown:0.0.1-alpine
+az group create -n countdown -l eastus
+az container create --name projcount --image tripdubroot/countdown:0.0.1-alpine --cpu 1 --memory 1 --ip-address public -g countdown
